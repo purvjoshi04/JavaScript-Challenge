9bf3ac4c0551cc2ef472ca8aed0b331bfb18404f
Purv Joshi <purvadjoshi@gmail.com>
2024-07-18 23:50:33 +0530

    Day-5 Javascript Functions Challenge

--- Day-5/index.js
+++ Day-5/index.js
@@ -26,20 +26,20 @@ console.log(numSquare(4));
 
 // Task-3: Write a function expression to find the maximum of two numbers and log the result to the console.
 
-const maxNum =  function (numOne, numTwo) {
-    if (numOne > numTwo) {
-        return `The maximum number is ${numOne}.`
-    }else {
-        return `The maximum number is ${numTwo}.`
-    }
-}
-console.log(maxNum(1,3));
+const maxNum = function (numOne, numTwo) {
+  if (numOne > numTwo) {
+    return `The maximum number is ${numOne}.`;
+  } else {
+    return `The maximum number is ${numTwo}.`;
+  }
+};
+console.log(maxNum(1, 3));
 
 // Task-4: Write a function expression to concatenate two strings and return the result.
 
 const concateStr = function (oneStr, twoStr) {
-    return `${oneStr + twoStr}`
-} 
+  return `${oneStr + twoStr}`;
+};
 console.log(concateStr("Hello ", "World"));
 
 // Activity-3: Arrow Functions
@@ -47,20 +47,57 @@ console.log(concateStr("Hello ", "World"));
 // Task-5: Write an arrow function to calculate the sum of two numbers and return the result.
 
 sumNun = (one, two) => {
-    return `The sum of two number is ${one + two}.`
-}
-console.log(sumNun(20,10));
+  return `The sum of two number is ${one + two}.`;
+};
+console.log(sumNun(20, 10));
 
 // Task-6: Write an arrow function to check if a string contains a specific character and return a boolean value.
 
-let vowels = ['a', 'e', 'i', 'o', 'u'];
+let vowels = ["a", "e", "i", "o", "u"];
 
 findChar = (str) => {
-  return vowels.some(vowel => str.toLowerCase().includes(vowel))
-}
-console.log(findChar('Hello'))
-
+  return vowels.some((vowel) => str.toLowerCase().includes(vowel));
+};
+console.log(findChar("Hello"));
 
 // Activity-4: Function Parameters and Default Values
 
-// Task-7: Write a function that takes two paramters and retunrs their product. Provide a default value for the seconde parameter.
+// Task-7: Write a function that takes two paramters and returns their product. Provide a default value for the seconde parameter.
+
+function parameter(a, b = 10) {
+  return a * b;
+}
+console.log(parameter(10));
+
+// Task-8: Write a function that takes a person's name and age returns a greeting message. Provide a default value for the age.
+
+function greet(name, age = 20) {
+  return `Hello ${name} your age is ${age}.`;
+}
+console.log(greet("Naman"));
+
+// Activity-5: Higher-Order Functions
+
+// Task-9: Write a higher-order function that takes a function and a number, and calls the function that many times.
+
+function higherOrder(fn, times) {
+  for (let i = 0; i < times; i++) {
+    fn();
+  }
+}
+function func() {
+  console.log("Hello World");
+}
+higherOrder(func, 3);
+
+// Task-10: Write a higher-order function that takes two functions and a value, applies the first function to the value, and then applies the second function to the result.
+
+const compose = (f, g) => x => g(f(x));
+
+const double = (x) => x * 2;
+const square = (x) => x ** 2;
+
+const doubleThenSuquare = compose(double, square);
+
+const result = doubleThenSuquare(3);
+console.log(result);
