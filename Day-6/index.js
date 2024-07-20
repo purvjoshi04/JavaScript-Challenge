de0a89f33ea82db829a646323e8d8ab00e2172bc
Purv Joshi <purvadjoshi@gmail.com>
2024-07-20 11:18:31 +0530

    Day-6 Javascript array creation and access challenge

---
+++ Day-6/index.js
@@ -0,0 +1,10 @@
+// Activity-1 : Array Creation and Access
+
+// Task-1: Create an array of numbers from 1 to 5 and log the array to the console.
+
+let numArray = [1, 2, 3, 4, 5];
+console.log(numArray);
+
+// Task-2 Access the first and last elements of the array and log them to the console.
+
+console.log(`First element ${numArray[0]} and last element ${numArray[4]}.`)
